7b5983362ee4fd59778c9328d9db59603440a6bf
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-12-06 17:03:55 -0500

    curl data into data directory

--- curl_data.sh
+++ curl_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-curl http://media.mongodb.org/zips.json > zips.json
-curl https://raw.githubusercontent.com/mongodb/docs-assets/primer-dataset/dataset.json > rest.json
+mkdir data
+curl http://media.mongodb.org/zips.json > data/zips.json
+curl https://raw.githubusercontent.com/mongodb/docs-assets/primer-dataset/dataset.json > data/rest.json
 
-mongoimport --db test --collection rest --drop --file rest.json
-mongoimport --db test --collection zips --drop --file zips.json
+mongoimport --db test --collection rest --drop --file data/rest.json
+mongoimport --db test --collection zips --drop --file data/zips.json
